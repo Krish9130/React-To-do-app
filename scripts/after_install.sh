8fcda3c9c5248e23753adf0acafbdd5dd86c13ec
Krushna Sonawane <105139116+Krish9130@users.noreply.github.com>
2025-09-08 20:35:36 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-cd /opt/tet/todo/
+cd /opt/react/build/
 cp -r * /var/www/html
 cd /var/www/html
 #mv index.nginx-debian.html ../
 service nginx reload
 
-#npm install pm2@latest -g
-#pm2 start npm --name "todo-react-app" -- start
